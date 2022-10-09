47e72bee780c0f95cd82312de137f770b568ce01
inbux <inbux.development@gmail.com>
2022-10-09 13:08:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+![Logo](admin/ethersex.png)
+
+# ioBroker.ethersex
+
+[![Downloads](https://img.shields.io/npm/dm/iobroker.ethersex.svg)](https://www.npmjs.com/package/iobroker.ethersex)
+![Number of Installations](https://iobroker.live/badges/ethersex-installed.svg)
+
+## ethersex adapter for ioBroker
+
+Reading 1wire sensor data from ethersex device
+
+## Features
+
+[Ethersex](https://www.ethersex.de/) is a community driven firmware with network support for 8-bit AVR microcontrollers, like AVR 8,16,32,64,128,644 and many more.
+
+This adapter connects to an Ethesex device and reads the temperatures of all connected 1wire devices by using the ECMD protocol.
+
+## Configuration
+
+Just setup the IP address of the server (Ethersex device) and the polling interval.
+
+![](/docs/ethersex_setup.png)
+
+The adapter will create objects for all connected 1wire devices.
+
+![](/docs/ethersex_objects.png)
+
+## Changelog
+
+<!--
+	Placeholder for the next version (at the beginning of the line):
+	### **WORK IN PROGRESS**
+-->
+
+### **WORK IN PROGRESS**
+
+### 0.0.1 (2022-10-09)
+
+-   (inbux) initial release
+
+## License
+
+MIT License
+
+Copyright (c) 2022 inbux <inbux.development@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ main.js
@@ -0,0 +1,171 @@
+'use strict';
+const axios = require('axios').default;
+
+/*
+ * Created with @iobroker/create-adapter v2.3.0
+ */
+
+// The adapter-core module gives you access to the core ioBroker functions
+// you need to create an adapter
+const utils = require('@iobroker/adapter-core');
+var interval;
+
+// Load your modules here, e.g.:
+// const fs = require("fs");
+
+class Ethersex extends utils.Adapter {
+	/**
+	 * @param {Partial<utils.AdapterOptions>} [options={}]
+	 */
+	constructor(options) {
+		super({
+			...options,
+			name: 'ethersex',
+		});
+
+		this.netio = null;
+		this.apiConnected = false;
+		this.devices = [];
+		this.pollingInterval = 5 * 60000;
+
+		this.on('ready', this.onReady.bind(this));
+		//this.on("stateChange", this.onStateChange.bind(this));
+		// this.on("objectChange", this.onObjectChange.bind(this));
+		// this.on("message", this.onMessage.bind(this));
+		this.on('unload', this.onUnload.bind(this));
+	}
+
+	/**
+	 * Is called when databases are connected and adapter received configuration.
+	 */
+	async onReady() {
+		// Initialize your adapter here
+
+		// The adapters config (in the instance object everything under the attribute "native") is accessible via
+		// this.config:
+
+		this.connectToServer();
+		if (this.config.interval) this.pollingInterval = parseInt(this.config.interval) * 60000;
+		this.log.info('config Polling interval: ' + this.pollingInterval);
+
+		interval = setInterval(() => {
+			if (!this.apiConnected) {
+				this.connectToServer();
+			} else {
+				for (let i in this.devices) {
+					this.readSensorData(this.devices[i]);
+				}
+			}
+		}, this.pollingInterval);
+	}
+	// Connect to Server
+	async connectToServer() {
+		this.log.info('config Server: ' + this.config.server);
+		await this.setStateAsync('info.connection', { val: false, ack: true });
+
+		this.netio = axios.create({
+			baseURL: `http://${this.config.server}/`,
+			insecureHTTPParser: true,
+			timeout: 5000,
+			headers: {
+				'User-Agent': 'Mozilla',
+			},
+		});
+
+		try {
+			const response = await this.netio.get('/ecmd?version');
+			if (response.status == 200) {
+				this.log.info('Version ' + response.data);
+				await this.setStateAsync('info.connection', { val: true, ack: true });
+				await this.setStateAsync('info.version', { val: response.data, ack: true });
+				this.apiConnected = true;
+				this.readSensors();
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	// Get available 1wire sensors
+	async readSensors() {
+		try {
+			// @ts-ignore
+			const response = await this.netio.get('/ecmd?1w list');
+			this.devices = response.data.split('\n');
+			this.log.debug(this.devices);
+			this.devices.length = this.devices.length - 2; // OK und leere Zeile loeschen
+
+			for (let i in this.devices) {
+				this.log.debug('ID: ' + this.devices[i]);
+				await this.setObjectNotExistsAsync(this.devices[i], {
+					type: 'state',
+					common: {
+						name: this.devices[i],
+						type: 'number',
+						role: 'value.temperature',
+						unit: '°C',
+						read: true,
+						write: false,
+					},
+					native: {},
+				});
+			}
+
+			for (let i in this.devices) {
+				this.readSensorData(this.devices[i]);
+			}
+		} catch (err) {
+			// Set device offline
+			this.apiConnected = false;
+			await this.setStateAsync('info.connection', { val: this.apiConnected, ack: true });
+
+			this.log.error(err);
+		}
+	}
+
+	// Get available 1wire sensors
+	async readSensorData(sensorID) {
+		try {
+			// @ts-ignore
+			this.log.debug('Reading ' + sensorID);
+			await this.netio.get('/ecmd?1w convert ' + sensorID);
+			const response = await this.netio.get('/ecmd?1w get ' + sensorID);
+
+			this.log.debug(sensorID + ' = ' + response.data + ' °C');
+
+			await this.setStateAsync(sensorID, { val: response.data, ack: true });
+		} catch (err) {
+			// Set device offline
+			this.apiConnected = false;
+			await this.setStateAsync('info.connection', { val: this.apiConnected, ack: true });
+
+			this.log.error(err);
+		}
+	}
+
+	/**
+	 * Is called when adapter shuts down - callback has to be called under any circumstances!
+	 * @param {() => void} callback
+	 */
+	onUnload(callback) {
+		try {
+			// Here you must clear all timeouts or intervals that may still be active
+			clearInterval(interval);
+
+			callback();
+		} catch (e) {
+			callback();
+		}
+	}
+}
+
+if (require.main !== module) {
+	// Export the constructor in compact mode
+	/**
+	 * @param {Partial<utils.AdapterOptions>} [options={}]
+	 */
+	module.exports = (options) => new Ethersex(options);
+} else {
+	// otherwise start the instance directly
+	new Ethersex();
+}
